53d6bd4423d4c61878c370603a661a00aae5c051
adm1255 <96832058+adm1255@users.noreply.github.com>
2023-01-11 21:31:18 +0600

    Массивы

---
+++ kanat.js
@@ -0,0 +1,15 @@
+let color1 = "orange";
+let color2 = "red";
+let color3 = "black";
+let color4 = "blue";
+let color5 = "green";
+let color6 = "yellow";
+let color7 = "aqua";
+
+const rainbowColord = ["orange", "red", "black", "blue", "green", "yellow", "aqua" ];
+
+console.log(rainbowColord);
+
+let numbers = [100, 1, 10, 35]
+
+console.log(numbers)
